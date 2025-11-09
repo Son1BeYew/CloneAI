e402a4c4e58819ae55dc4b91ddda15935acf4dea
Son1BeYew <142385993+Son1BeYew@users.noreply.github.com>
2025-11-09 22:11:03 +0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -69,8 +69,6 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(` Server running on port ${PORT}`);
 });
